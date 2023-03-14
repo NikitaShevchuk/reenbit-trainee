44cada89ccefd6ab0835d885146cc7319603a946
Nikita Shevhcuk <shevchuk.nikita.gh@gmail.com>
2023-03-14 18:09:27 +0200

    Add logut action test

--- src/redux/slices/__test__/profileSlice.test.ts
+++ src/redux/slices/__test__/profileSlice.test.ts
@@ -12,4 +12,13 @@ describe('Profile slice', () => {
         store.dispatch(profileSlice.actions.setAccessToken('test_token'));
         expect(store.getState().profileSlice.access_token).toBe('test_token');
     });
+
+    it('Should clear all authorization data from state', () => {
+        store.dispatch(profileSlice.actions.logout());
+        expect(store.getState().profileSlice.isAuthorizedWithFacebook).toBe(false);
+        expect(store.getState().profileSlice.isAuthorizedWithGoogle).toBe(false);
+        expect(store.getState().profileSlice.isAuthorized).toBe(false);
+        expect(store.getState().profileSlice.access_token).toBe(null);
+        expect(store.getState().profileSlice.profile).toBe(null);
+    });
 });
